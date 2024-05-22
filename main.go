f48067420500c033d8ed218c4a00a9d5cad39e71
Zograf <lazzarmilanovic@gmail.com>
2024-05-22 17:36:42 +0200

    Add: Simple proxy tidied up

--- main.go
+++ main.go
@@ -1,39 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"net/http"
-)
-
-func proxy_hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("Forwarding request...")
-	httpClient := &http.Client{}
-	proxyReq, _ := http.NewRequest(req.Method, "http://localhost:1234/hello", nil)
-	resp, err := httpClient.Do(proxyReq)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadGateway)
-		return
-	}
-	bodyBytes, _ := io.ReadAll(resp.Body)
-	w.WriteHeader(resp.StatusCode)
-	fmt.Println("Request executed: " + string(bodyBytes))
-	io.Copy(w, resp.Body)
-	resp.Body.Close()
-}
-
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("Service executing request...")
-	fmt.Fprintf(w, "Hooray!")
-}
-
-func make_proxy() {
-	http.HandleFunc("/proxy", proxy_hello)
-	http.ListenAndServe(":8080", nil)
-}
+import "github.com/zograf/gobserve/simple_proxy"
 
 func main() {
-	go make_proxy()
-	http.HandleFunc("/hello", hello)
-	http.ListenAndServe(":1234", nil)
+	go simple_proxy.Make_proxy()
+	simple_proxy.Make_http_server()
 }
